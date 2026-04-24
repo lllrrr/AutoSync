0bf0e95dfe2a8cb2ab31ff329b1c210b40439782
lllrrr <31834680+lllrrr@users.noreply.github.com>
2026-04-24 18:29:18 +0800

    Update master.sh

--- .github/diy/master.sh
+++ .github/diy/master.sh
@@ -139,6 +139,10 @@ git clone --depth 1 https://github.com/sirpdboy/luci-app-eqosplus
 sed -i 's#..translate("Suggested feedback:.*##g' luci-app-eqosplus/luasrc/model/cbi/eqosplus.lua
 git clone --depth 1 https://github.com/sirpdboy/luci-app-netdata netdata1 && mv -n netdata1/luci-app-netdata ./; rm -rf netdata1
 git clone --depth 1 https://github.com/huchd0/luci-app-netwiz netwiz1 && mv -n netwiz1/luci-app-netwiz ./; rm -rf netwiz1
+chmod +x luci-app-netwiz/root/usr/libexec/rpcd/netwiz
+chmod +x luci-app-netwiz/root/usr/libexec/netwiz-autodetect.sh
+chmod +x luci-app-netwiz/root/usr/libexec/netwiz-rollback.sh
+chmod +x luci-app-netwiz/root/etc/init.d/netwiz-recovery
 git clone --depth 1 https://github.com/sirpdboy/luci-app-netwizard netwizard1 && mv -n netwizard1/luci-app-netwizard ./; rm -rf netwizard1
 #sed -i 's#..translate("For specific usage,.*# )#g' luci-app-netwizard/luasrc/model/cbi/netwizard/netwizard.lua
 git clone --depth 1 https://github.com/sirpdboy/netspeedtest netspeedtest1 && mv -n netspeedtest1/{*speedtest*,homebox} ./; rm -rf netspeedtest1
