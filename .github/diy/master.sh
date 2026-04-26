232fdb55098543d45438cff9d9889d41f7d3346c
lllrrr <31834680+lllrrr@users.noreply.github.com>
2026-04-26 21:54:42 +0800

    Update master.sh

--- .github/diy/master.sh
+++ .github/diy/master.sh
@@ -144,6 +144,7 @@ chmod +x luci-app-netwiz/root/usr/libexec/netwiz-autodetect.sh
 chmod +x luci-app-netwiz/root/usr/libexec/netwiz-monitor-loop.sh
 chmod +x luci-app-netwiz/root/etc/init.d/netwiz-monitor
 chmod +x luci-app-netwiz/root/etc/init.d/netwiz-recovery
+sed -i "s#Netwiz 网络设置向导#网络设置向导#g" luci-app-netwiz/po/zh-Hans/netwiz.po
 git clone --depth 1 https://github.com/sirpdboy/luci-app-netwizard netwizard1 && mv -n netwizard1/luci-app-netwizard ./; rm -rf netwizard1
 #sed -i 's#..translate("For specific usage,.*# )#g' luci-app-netwizard/luasrc/model/cbi/netwizard/netwizard.lua
 git clone --depth 1 https://github.com/sirpdboy/netspeedtest netspeedtest1 && mv -n netspeedtest1/{*speedtest*,homebox} ./; rm -rf netspeedtest1
