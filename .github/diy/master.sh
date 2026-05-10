92c048560725246b3f235d0b41a2a905aefeddf8
lllrrr <31834680+lllrrr@users.noreply.github.com>
2026-05-10 18:07:14 +0800

    fix bandix

--- .github/diy/master.sh
+++ .github/diy/master.sh
@@ -240,7 +240,7 @@ git clone --depth 1 https://github.com/timsaya/luci-app-bandix bandix2 && mv -n 
 #sed -i "s#Bandix 连接监控#连接监控#g" luci-app-bandix/po/zh_Hans/bandix.po
 #sed -i "s#Bandix 流量监控#流量监控#g" luci-app-bandix/root/usr/share/luci/status/bandix.json
 sed -i "s#Bandix#流量监控#g" luci-app-bandix/root/usr/share/luci/menu.d/luci-app-bandix.json
-sed -i '634,642d' luci-app-bandix/htdocs/luci-static/resources/view/bandix/settings.js
+#sed -i '634,642d' luci-app-bandix/htdocs/luci-static/resources/view/bandix/settings.js
 #sed -i '670,676d' luci-app-bandix/htdocs/luci-static/resources/view/bandix/settings.js
 git clone --depth 1 https://github.com/timsaya/openwrt-bandix-plus bandixp1 && mv -n bandixp1/*bandix-plus ./bandix-plus; rm -rf bandixp1
 git clone --depth 1 https://github.com/timsaya/luci-app-bandix-plus bandixp2 && mv -n bandixp2/*bandix-plus ./; rm -rf bandixp2
