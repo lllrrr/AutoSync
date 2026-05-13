412b126a27145631ff017f37ec51310c91fd73db
lllrrr <31834680+lllrrr@users.noreply.github.com>
2026-05-13 11:27:43 +0800

    Update master.sh

--- .github/diy/master.sh
+++ .github/diy/master.sh
@@ -301,6 +301,7 @@ git clone --depth 1 https://github.com/qimaoww/luci-app-lanspeed lanspeed1 && mv
 git clone --depth 1 https://github.com/Antecer/luci-app-devgate devgate1 && mv -n devgate1/*devgate ./; rm -rf devgate1
 git clone --depth 1 https://github.com/mokanove/luci-app-pm
 git clone --depth 1 https://github.com/Boos4721/luci-app-hermes
+git clone --depth 1 https://github.com/small-white-rabbit/luci-app-Devicemaster
 
 git_sparse_clone openwrt-25.12 "https://github.com/immortalwrt/packages" "immpack" net/cdnspeedtest net/dae net/daed net/dufs net/rustdesk-server net/zerotier net/gost net/n2n
 git_sparse_clone openwrt-25.12 "https://github.com/immortalwrt/luci" "immluci" applications/luci-app-autoreboot applications/luci-app-dae applications/luci-app-daed applications/luci-app-dufs applications/luci-app-rustdesk-server applications/luci-app-zerotier applications/luci-app-gost applications/luci-app-n2n
