e4e50bfb11881e7a7b863d089da7ebdf1cab53fe
lllrrr <31834680+lllrrr@users.noreply.github.com>
2026-07-02 21:24:29 +0800

    luci-app-oxidns

--- .github/diy/master.sh
+++ .github/diy/master.sh
@@ -326,6 +326,7 @@ git clone --depth 1 https://github.com/hello-yunshu/luci-app-cloudflare-ip cloud
 git clone --depth 1 https://github.com/r1172464137/luci-app-devman devman2 && mv -n devman2/*devman ./; rm -rf devman2
 git clone --depth 1 https://github.com/hello-yunshu/luci-app-nginx-manager
 git clone --depth 1 https://github.com/hello-yunshu/luci-app-upnp-nat-relay
+git clone --depth 1 https://github.com/svenshi/luci-app-oxidns
 
 git_sparse_clone openwrt-25.12 "https://github.com/immortalwrt/packages" "immpack" net/cdnspeedtest net/dae net/daed net/dufs net/rustdesk-server net/zerotier net/gost net/n2n
 git_sparse_clone openwrt-25.12 "https://github.com/immortalwrt/luci" "immluci" applications/luci-app-autoreboot applications/luci-app-dae applications/luci-app-daed applications/luci-app-dufs applications/luci-app-rustdesk-server applications/luci-app-zerotier applications/luci-app-gost applications/luci-app-n2n
