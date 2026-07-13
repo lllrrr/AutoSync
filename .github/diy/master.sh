190e1eae847d179cf26d78ed0f23e1ff4c59330f
lllrrr <31834680+lllrrr@users.noreply.github.com>
2026-07-13 12:31:33 +0800

    Update master.sh

--- .github/diy/master.sh
+++ .github/diy/master.sh
@@ -332,6 +332,7 @@ git clone --depth 1 https://github.com/c2h2/luci-app-photondns photondns1 && mv 
 git clone --depth 1 https://github.com/luanmuc/luci-app-systools
 git clone --depth 1 https://github.com/1391959853/luci-app-usbmodem
 git clone --depth 1 https://github.com/chmod740/luci-app-monitor
+git clone --depth 1 https://github.com/kinmeic/luci-app-bypass
 
 git_sparse_clone openwrt-25.12 "https://github.com/immortalwrt/packages" "immpack" net/cdnspeedtest net/dae net/daed net/dufs net/rustdesk-server net/zerotier net/gost net/n2n
 git_sparse_clone openwrt-25.12 "https://github.com/immortalwrt/luci" "immluci" applications/luci-app-autoreboot applications/luci-app-dae applications/luci-app-daed applications/luci-app-dufs applications/luci-app-rustdesk-server applications/luci-app-zerotier applications/luci-app-gost applications/luci-app-n2n
