881f6e8f71afed4ea7e657ceae60a518cf70cb6b
lllrrr <31834680+lllrrr@users.noreply.github.com>
2026-08-02 17:58:57 +0800

    Update master.sh

--- .github/diy/master.sh
+++ .github/diy/master.sh
@@ -118,7 +118,7 @@ sed -i 's#Tokio framework. "#Tokio framework. ")#g' luci-app-easytier/luasrc/mod
 sed -i '5,10d' luci-app-easytier/luasrc/model/cbi/easytier_status.lua
 sed -i '679,692d' luci-app-easytier/luasrc/view/easytier/easytier_cli.htm
 git clone --depth 1 https://github.com/lmq8267/luci-app-vnt vnt1 && mv -n vnt1/*vnt* ./; rm -rf vnt1
-#@sed -i '5d' luci-app-vnt/luasrc/model/cbi/vnt.lua
+sed -i '5d' luci-app-vnt/luasrc/model/cbi/vnt.lua
 #git clone --depth 1 https://github.com/whzhni1/luci-app-vnt vnt2 && mv -n vnt2/*vnt* ./; rm -rf vnt2
 #git clone --depth 1 https://github.com/weicaixian86/luci-app-vnt2 vnt2 && mv -n vnt2/*vnt2 ./; rm -rf vnt2
 git clone --depth 1 https://github.com/sirpdboy/luci-app-taskplan taskplan1 && mv -n taskplan1/luci-app-taskplan ./; rm -rf taskplan1
